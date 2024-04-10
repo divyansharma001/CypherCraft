1a602970a6607cdddc6eca3d2cfb03bfc91f7375
Divyansh Sharma <divyanshsharma4507@gmail.com>
2024-04-10 23:00:59 +0530

    apos error

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,88 +1,87 @@
-
 function Footer() {
-    return (
-      <footer className="bg-black text-gray-400 py-12">
-          <div className="max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 px-4 sm:px-6 lg:px-8">
-          <div>
-            <h2 className="text-white text-lg font-semibold mb-4">About Us</h2>
-            <p className="mb-4">
-            Welcome to CypherCraft, your go-to platform for mastering the intricacies of technology. At CypherCraft, 
-            we're dedicated to providing top-notch education in fields like Data Structures and Algorithms (DSA), 
-            Software Development (Dev), Artificial Intelligence and Machine Learning (AIML), and Internet of Things (IoT). 
-            Join our vibrant community of tech enthusiasts and embark on your journey to tech mastery today!
-            </p>
-          </div>
-          <div>
-            <h2 className="text-white text-lg font-semibold mb-4">Quick Links</h2>
-            <ul>
-              <li>
-                <a
-                  href="#"
-                  className="hover:text-white transition-colors duration-300"
-                >
-                  Home
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="hover:text-white transition-colors duration-300"
-                >
-                  About
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="hover:text-white transition-colors duration-300"
-                >
-                  Courses
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  className="hover:text-white transition-colors duration-300"
-                >
-                  Contact
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div>
-            <h2 className="text-white text-lg font-semibold mb-4">Follow Us</h2>
-            <div className="flex space-x-4">
+  return (
+    <footer className="bg-black text-gray-400 py-12">
+        <div className="max-w-6xl mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 px-4 sm:px-6 lg:px-8">
+        <div>
+          <h2 className="text-white text-lg font-semibold mb-4">About Us</h2>
+          <p className="mb-4">
+          Welcome to CypherCraft, your go-to platform for mastering the intricacies of technology. At CypherCraft, 
+          we&apos;re dedicated to providing top-notch education in fields like Data Structures and Algorithms (DSA), 
+          Software Development (Dev), Artificial Intelligence and Machine Learning (AIML), and Internet of Things (IoT). 
+          Join our vibrant community of tech enthusiasts and embark on your journey to tech mastery today!
+          </p>
+        </div>
+        <div>
+          <h2 className="text-white text-lg font-semibold mb-4">Quick Links</h2>
+          <ul>
+            <li>
               <a
                 href="#"
                 className="hover:text-white transition-colors duration-300"
               >
-                Facebook
+                Home
               </a>
+            </li>
+            <li>
               <a
                 href="#"
                 className="hover:text-white transition-colors duration-300"
               >
-                Twitter
+                About
               </a>
+            </li>
+            <li>
               <a
                 href="#"
                 className="hover:text-white transition-colors duration-300"
               >
-                Instagram
+                Courses
               </a>
-            </div>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="hover:text-white transition-colors duration-300"
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <h2 className="text-white text-lg font-semibold mb-4">Follow Us</h2>
+          <div className="flex space-x-4">
+            <a
+              href="#"
+              className="hover:text-white transition-colors duration-300"
+            >
+              Facebook
+            </a>
+            <a
+              href="#"
+              className="hover:text-white transition-colors duration-300"
+            >
+              Twitter
+            </a>
+            <a
+              href="#"
+              className="hover:text-white transition-colors duration-300"
+            >
+              Instagram
+            </a>
           </div>
-          <div>
-            <h2 className="text-white text-lg font-semibold mb-4">Contact Us</h2>
-            <p>New Delhi, India</p>
-            <p>Delhi 10001</p>
-            <p>Email: info@cyphercraft.com</p>
-            <p>Phone: (123) 456-7890</p>
-          </div>
-          </div>
-          <p className="text-center text-xs pt-8">© 2024 Cyphercraft. All rights reserved.</p>
-      </footer>
-    )
-  }
-  
-  export default Footer
+        </div>
+        <div>
+          <h2 className="text-white text-lg font-semibold mb-4">Contact Us</h2>
+          <p>New Delhi, India</p>
+          <p>Delhi 10001</p>
+          <p>Email: info@cyphercraft.com</p>
+          <p>Phone: (123) 456-7890</p>
+        </div>
+        </div>
+        <p className="text-center text-xs pt-8">© 2024 Cyphercraft. All rights reserved.</p>
+    </footer>
+  )
+}
+
+export default Footer;
